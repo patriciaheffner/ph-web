7fba5fabd9b22de1bce1a6d734fa2827204e1d60
patricia heffner <poheffner@gmail.com>
2017-05-14 13:26:34 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Print } from './print';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string;
+  myPrint: string;
+  prints: any[];
+
+  constructor() {
+    this.title = 'Hello there!';
+    this.prints = [
+      new Print (1, 'Polka Dot'),
+      new Print (2, 'Plaid'),
+      new Print (3, 'Stripes'),
+      new Print (4, 'Solid')
+    ];
+    this.myPrint = this.prints[0];
+  }
 }
---
+++ src/app/print.ts
@@ -0,0 +1,5 @@
+export class Print {
+  constructor(
+    public id: number,
+    public name: string) { }
+}
